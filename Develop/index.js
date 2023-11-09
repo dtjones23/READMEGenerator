2b827a9d20648e9b4268e8cedf7908da403230ca
DeAngelo Jones <deangelojones0708@gmail.com>
2023-11-08 21:38:28 -0600

    added README questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,57 @@
-// TODO: Include packages needed for this application
+// Added node mods
+const fs = require('fs')
+const inquirer = require('inquirer')
+
+// Add questions
+function init() {
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of your project?'
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Give a description.'
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'What are the installation instructions?'
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Describle usage information'
+            },
+            {
+                type: 'input',
+                name: 'contribution',
+                message: 'List contributors (if any).'
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: 'Explain the test instructions?'
+            }
+        ])
+        .then((answers) => {
+            const userInput = question(answers);
+            console.log(userInput)
+        }
+        )
+}
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() { }
 
 // Function call to initialize app
 init();
